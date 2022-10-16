c6bf34cb42f80f6e98ff2db43cd238f975b47681
akcenurt <truneckaj@gmail.com>
2022-10-16 19:48:52 +0200

    scrolling tests added

--- cz.akcenurt/src/test/java/ScrollTest.java
+++ cz.akcenurt/src/test/java/ScrollTest.java
@@ -1,5 +1,4 @@
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -7,8 +6,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 import java.time.Duration;
 import java.util.List;
@@ -16,30 +14,58 @@ import java.util.List;
 public class ScrollTest {
 
     private WebDriver driver;
-    private final String BASE_URL = "http://localhost/";
+    private final String BASE_URL = "http://localhost/tabulka.php";
 
     @Before
     public void setUP(){
         System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.get(BASE_URL);
     }
 
     @Test
     public void testScrollTo(){
 
-        driver.get(BASE_URL + "tabulka.php");
         WebElement lastRow = driver.findElement(By.xpath("//table//tbody//tr[last()]"));
                 // např. druhý řádek xpath: //table//tbody//tr[2]]
         JavascriptExecutor js = ((JavascriptExecutor) driver);
         js.executeScript("arguments[0].scrollIntoView(true)", lastRow); // to true se vztahuje k align to top parametru, tedy naskroluje tak, aby byl řádek nahoře
 
+    }
 
+    @Test
+    public void scrollByXPixelsTest(){
 
+        JavascriptExecutor js = ((JavascriptExecutor) driver);
 
+        for (int i = 0; i < 4; i++) {
+            js.executeScript("window.scrollBy(0,200)");
+        }
 
     }
 
+    @Test
+    public void scrollToTheUnknownEnd(){
+
+        JavascriptExecutor js = ((JavascriptExecutor) driver);
+
+        long bodyHeight = (long) js.executeScript("return document.body.scrollHeight");
+//        System.out.println(scrollHeight);
+        js.executeScript("window.scrollBy(0," + bodyHeight + ")");
+
+    }
+    @Test
+    public void scrollByOnePixel(){
+
+        JavascriptExecutor js = ((JavascriptExecutor) driver);
+
+        long bodyHeight = (long) js.executeScript("return document.body.scrollHeight");
+        for (int i = 0; i < bodyHeight; i++) {
+            js.executeScript("window.scrollBy(0,1)");
+        }
+
+    }
 
 
     @After
